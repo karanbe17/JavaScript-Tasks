03f72d854d2f1c2d14864e20a990deb539556b99
Karan <karanbe17@gmail.com>
2026-01-29 15:54:46 +0530

    Practiced extra problems

---
+++ All_tasks/extra_tasks/task3.js
@@ -0,0 +1,20 @@
+// 1) const btn = document.getElementById("my-btn");
+// btn.addEventListener("click", function(){
+//     console.log("Button is Clicked!");
+// });
+
+// 2) const b = document.getElementById("box");
+
+// b.addEventListener("mousemove", function(e){
+//     console.log(`X: ${e.clientX} + ${e.clientY}`)
+// })
+
+// 3) const link = document.getElementById("dangerLink");
+// const msg = document.getElementById("newMsg");
+
+// link.addEventListener('click', function(e){
+//     e.preventDefault();
+//     console.log("Navigation failed!");
+//     msg.innerText="You are safe!";
+// });
+
