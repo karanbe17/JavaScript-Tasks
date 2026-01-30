9df69b669e148feec6b2cdc61b41793b7f34eb7f
Karan <karanbe17@gmail.com>
2026-01-30 15:48:06 +0530

    Practiced extra problems

--- All_tasks/extra_tasks/task3.js
+++ All_tasks/extra_tasks/task3.js
@@ -18,3 +18,47 @@
 //     msg.innerText="You are safe!";
 // });
 
+// 4) const list = document.getElementById("lst");
+// const addbtn = document.getElementById("but");
+
+// list.addEventListener('click', function(e){
+//     if (e.target.tagName === "LI"){
+//         console.log("You clicked: ", e.target.innerText);
+
+//     e.target.style.textDecoration = "line-through";
+//     e.target.style.color = "red";
+//     }
+// });
+
+// addbtn.addEventListener('click', function(){
+//     const newItem = document.createElement("li");
+//     newItem.innerText = "New Task " + (Math.random() * 100).toFixed(0);
+
+//     list.appendChild(newItem);
+// });
+
+const grid = document.getElementById("mainBox");
+
+let currentTurn = "X";
+
+grid.addEventListener("click", function(e){
+    if (e.target.classList.contains("box")){
+        if (e.target.innerText === ""){
+            e.target.innerText = currentTurn;
+
+            if (currentTurn === "X"){
+                e.target.style.backgroundColor = "ffcccc";
+            } else{
+                e.target.style.backgroundColor = "#ccffcc"
+            }
+
+            if (currentTurn = "X") {
+                currentTurn = "O";
+            } else {
+                currentTurn = "X"; 
+            }
+
+            console.log("Next Turn: ", currentTurn);
+        }
+    }
+})
