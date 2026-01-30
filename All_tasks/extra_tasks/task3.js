4fa9466d8e01e2153d2b9bf5541c4bc95c60e001
Karan <karanbe17@gmail.com>
2026-01-30 15:52:21 +0530

    solved few errors

--- All_tasks/extra_tasks/task3.js
+++ All_tasks/extra_tasks/task3.js
@@ -1,42 +1,48 @@
-// 1) const btn = document.getElementById("my-btn");
-// btn.addEventListener("click", function(){
-//     console.log("Button is Clicked!");
-// });
+// 1) 
+const btn = document.getElementById("my-btn");
+btn.addEventListener("click", function(){
+    console.log("Button is Clicked!");
+});
 
-// 2) const b = document.getElementById("box");
+// 2) 
+const b = document.getElementById("box");
 
-// b.addEventListener("mousemove", function(e){
-//     console.log(`X: ${e.clientX} + ${e.clientY}`)
-// })
+b.addEventListener("mousemove", function(e){
+    console.log(`X: ${e.clientX} + ${e.clientY}`)
+})
 
-// 3) const link = document.getElementById("dangerLink");
-// const msg = document.getElementById("newMsg");
+// 3) 
+const link = document.getElementById("dangerLink");
+const msg = document.getElementById("newMsg");
 
-// link.addEventListener('click', function(e){
-//     e.preventDefault();
-//     console.log("Navigation failed!");
-//     msg.innerText="You are safe!";
-// });
+link.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log("Navigation failed!");
+    msg.innerText="You are safe!";
+});
 
-// 4) const list = document.getElementById("lst");
-// const addbtn = document.getElementById("but");
+// 4) 
+const list = document.getElementById("lst");
+const addbtn = document.getElementById("but");
 
-// list.addEventListener('click', function(e){
-//     if (e.target.tagName === "LI"){
-//         console.log("You clicked: ", e.target.innerText);
+list.addEventListener('click', function(e){
+    if (e.target.tagName === "LI"){
+        console.log("You clicked: ", e.target.innerText);
 
-//     e.target.style.textDecoration = "line-through";
-//     e.target.style.color = "red";
-//     }
-// });
+    e.target.style.textDecoration = "line-through";
+    e.target.style.color = "red";
+    }
+});
 
-// addbtn.addEventListener('click', function(){
-//     const newItem = document.createElement("li");
-//     newItem.innerText = "New Task " + (Math.random() * 100).toFixed(0);
+addbtn.addEventListener('click', function(){
+    const newItem = document.createElement("li");
+    newItem.innerText = "New Task " + (Math.random() * 100).toFixed(0);
 
-//     list.appendChild(newItem);
-// });
+    list.appendChild(newItem);
+});
 
+
+// 5) TIC TAC TOE 
 const grid = document.getElementById("mainBox");
 
 let currentTurn = "X";
